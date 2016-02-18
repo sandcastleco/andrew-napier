1979cb1dd43b48b6be9317ecfdf4c98b36b78548
Kevin McGillivray <kevin.mcgillivray@me.com>
2016-02-18 00:02:31 -0600

    Add hover

--- v3.1/js/scripts.js
+++ v3.1/js/scripts.js
@@ -1,7 +1,14 @@
-var navLink = document.getElementById("nav-link");
+var squareLink = document.getElementById("square-link");
 
-function clickHandler() {
-  console.log("click");
+function hoverHandler() {
+  console.log("hover");
+  TweenLite.to(["#curfew", "#curfew-caption"], 1, {opacity: 1, ease: Power2.easeOut});
 }
 
-navLink.addEventListener("mousedown", clickHandler);
+function offHandler() {
+  console.log("off");
+  TweenLite.to(["#curfew", "#curfew-caption"], 1, {opacity: 0, ease: Power2.easeOut});
+}
+
+squareLink.addEventListener("mouseover", hoverHandler);
+squareLink.addEventListener("mouseout", offHandler);
