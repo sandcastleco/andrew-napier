8b9587986f46864075918a18cfe3d90143e67018
Foster Douglas <foster@sandcastle.co>
2023-01-14 00:42:47 -0800

    static test

--- _site/js/data.js
+++
@@ -1,9 +0,0 @@
-var rootRef = new Firebase('https://andrew-napier.firebaseio.com');
-var quotesRef = rootRef.child("quotes");
-
-quotesRef.on("value", function(snapshot) {
-  quotes = snapshot.val();
-  keys = Object.keys(quotes);
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
--- thank-you/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: center
-title: Thank You
----
-
-<h1>Thank you!</h1>
-<a class="btn" href="/">Return to home</a>
