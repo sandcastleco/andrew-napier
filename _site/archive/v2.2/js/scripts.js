608f5d398348f6ebd29797a51da535488d45dfc4
Foster Douglas <foster@sandcastle.co>
2023-01-14 01:00:31 -0800

    static test 2

--- _site/archive/v2.2/js/scripts.js
+++
@@ -1,7 +0,0 @@
-var navLink = document.getElementById("nav-link");
-
-function clickHandler() {
-  console.log("click");
-}
-
-navLink.addEventListener("mousedown", clickHandler);
--- cutting-room-floor/jacob/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: Jacob Have I Loved
----
-
-<img style="margin-bottom: 2rem;" src="/img/jacob-1.png">
-<img src="/img/jacob-2.png">
